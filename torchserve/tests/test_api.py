753a4852575c47ee55d0eb2b5fbb79d4b47b3ebf
sandstromviktor <v.sandstrom@icloud.com>
2023-01-12 11:14:10 +0100

    added test

--- torchserve/tests/test_api.py
+++ torchserve/tests/test_api.py
@@ -1,8 +1,50 @@
+import pytest
 import requests
+import os
+from requests.exceptions import ConnectionError
 
 
-endpoint = "http://0.0.0.0:8080"
+def get_inference_url():
+    return "http://localhost:8080"
 
-X = {'inputs': 1.2}
-model_info = requests.post(endpoint+":predict",json=X, verify=False)
-model_info.json()
+def get_management_url():
+    return "http://localhost:8081"
+
+def get_metrics_url():
+    return "http://localhost:8082"
+
+def test_status():
+    try:
+        url = get_inference_url() + "/ping"
+        response = requests.get(url)
+        if response.status_code == 200:
+            assert True
+    except ConnectionError:
+        assert False
+
+def test_health():
+    try:
+        url = get_inference_url() + "/ping"
+        response = requests.get(url)
+        if response.json()['status'] == "Healthy":
+            assert True
+    except ConnectionError:
+        assert False
+
+def test_prediction():
+
+    url = get_inference_url() + "/predictions/cnn"
+    base_path = os.path.join(os.getcwd(), 'tests', 'test_data')
+    file_1 = {'data': open(os.path.join(base_path, '0.png'), 'rb')}
+    file_2 = {'data': open(os.path.join(base_path, '1.png'), 'rb')}
+    response = requests.post(url, files=file_1)
+    prediction = response.json()
+    assert prediction == 0
+
+    response = requests.post(url, files=file_2)
+    prediction = response.json()
+    assert prediction == 1
+
+
+
+
