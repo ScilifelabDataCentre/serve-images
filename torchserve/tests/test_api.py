fe53530090ebd498e18b13525b0d0a3a7a9917aa
sandstromviktor <v.sandstrom@icloud.com>
2023-01-17 10:53:21 +0100

    update with simpler test structure

--- torchserve/tests/test_api.py
+++ torchserve/tests/test_api.py
@@ -1,25 +1,52 @@
+import os
 import pytest
 import requests
-import os
-import random as rnd
 from requests.exceptions import ConnectionError
+import random as rnd
+import time
+import docker
 
 
-def get_inference_url():
-    return "http://torchserve:8080"
+def get_inference_url(container):
+    url = "http://{}:8080".format(container.attrs["NetworkSettings"]["Networks"]["bridge"]["IPAddress"])
+    return url
 
 
-def get_management_url():
-    return "http://torchserve:8081"
+def get_management_url(container):
+    url = "http://{}:8081".format(container.attrs["NetworkSettings"]["Networks"]["bridge"]["IPAddress"])
+    return url
 
 
-def get_metrics_url():
-    return "http://torchserve:8082"
+def get_metrics_url(container):
+    url = "http://{}:8082".format(container.attrs["NetworkSettings"]["Networks"]["bridge"]["IPAddress"])
+    return url
 
 
-def test_status():
+client = docker.from_env()
+container = client.containers.run(os.environ['IMAGE_NAME'], ports={'8080/tcp': 8080, 
+                                                                    '8081/tcp': 8081,
+                                                                    '8082/tcp': 8082,
+                                                                    '7070/tcp': 7070,
+                                                                    '7071/tcp': 7071,
+                                                                    }, detach=True)
+time.sleep(20)
+container.reload()
+
+
+def test_torchserve_status():
+    assert container.status == "running"
+
+def test_rstudio_ports():
+    assert container.ports["8080/tcp"][0]["HostPort"] == "8080"
+    assert container.ports["8081/tcp"][0]["HostPort"] == "8081"
+    assert container.ports["8082/tcp"][0]["HostPort"] == "8082"
+    assert container.ports["7070/tcp"][0]["HostPort"] == "7070"
+    assert container.ports["7071/tcp"][0]["HostPort"] == "7071"
+
+
+def test_torchserve_access():
     try:
-        url = get_inference_url() + "/ping"
+        url = get_inference_url(container) + "/ping"
         response = requests.get(url)
         if response.status_code == 200:
             assert True
@@ -29,7 +56,7 @@ def test_status():
 
 def test_health():
     try:
-        url = get_inference_url() + "/ping"
+        url = get_inference_url(container) + "/ping"
         response = requests.get(url)
         if response.json()["status"] == "Healthy":
             assert True
@@ -38,14 +65,14 @@ def test_health():
 
 
 def test_list_models():
-    url = get_management_url() + "/models"
+    url = get_management_url(container) + "/models"
     response = requests.get(url)
     assert response.json()["models"][0]["modelName"] == "cnn"
 
 
 def test_scale_workers():
     num_workers = rnd.randint(2, 6)
-    url = get_management_url() + "/models/cnn"
+    url = get_management_url(container) + "/models/cnn"
     data = {"min_worker": num_workers, "synchronous": "true"}
     response = requests.put(url, params=data)
     assert response.json()["status"] == "Workers scaled to {} for model: cnn".format(
@@ -55,9 +82,9 @@ def test_scale_workers():
 
 def test_prediction():
 
-    url = get_inference_url() + "/predictions/cnn"
-    file_1 = {"data": open(os.path.join("tests", "test_data", "0.png"), "rb")}
-    file_2 = {"data": open(os.path.join("tests", "test_data", "1.png"), "rb")}
+    url = get_inference_url(container) + "/predictions/cnn"
+    file_1 = {"data": open(os.path.join(os.getcwd(), "torchserve", "tests", "test_data", "0.png"), "rb")}
+    file_2 = {"data": open(os.path.join(os.getcwd(), "torchserve", "tests", "test_data", "1.png"), "rb")}
     response = requests.post(url, files=file_1)
     prediction = response.json()
     assert prediction == 0
@@ -65,3 +92,10 @@ def test_prediction():
     response = requests.post(url, files=file_2)
     prediction = response.json()
     assert prediction == 1
+
+
+def test_shutdown():
+    container.stop()
+    container.reload()
+    assert container.status == 'exited'
+    container.remove()
