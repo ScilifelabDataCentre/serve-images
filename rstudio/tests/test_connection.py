cce3e2b65ceb0562523178a6c2b4426e42a7fb35
sandstromviktor <v.sandstrom@icloud.com>
2023-01-24 13:39:47 +0100

    cleaned test script

--- rstudio/tests/test_connection.py
+++ rstudio/tests/test_connection.py
@@ -3,34 +3,58 @@ import time
 import docker
 import os
 
+# Settings
+PORT = 8787  # the RStudio port
+TIMEOUT_CALL = 5  # the timeout in seconds of the client request call
+
 
 client = docker.from_env()
 container = client.containers.run(
-    os.environ["IMAGE_NAME"], ports={"8787/tcp": 8787}, detach=True
+    os.environ["IMAGE_NAME"],
+    ports={
+        f"{PORT}/tcp": PORT,
+    },
+    detach=True,
 )
 time.sleep(20)
 container.reload()
 
 
 def test_rstudio_container():
+    """Test that the rstudio container is running."""
     assert container.status == "running"
 
 
 def test_rstudio_port():
-    assert container.ports["8787/tcp"][0]["HostPort"] == "8787"
+    """Test of the expected container port."""
+    assert len(container.ports) == 1, "There should be 1 port"
+    assert container.ports[f"{PORT}/tcp"][0]["HostPort"] == str(PORT)
 
 
 def test_rstudio_access():
-    rstudio_url = "http://{}:8787".format(
-        container.attrs["NetworkSettings"]["Networks"]["bridge"]["IPAddress"]
-    )
-    response = requests.get(rstudio_url, timeout=15)
-    assert response.status_code == 200
-    assert "RStudio" in response.text
+    """Test of basic communication with the container returns status 200 (OK)."""
+    url = _get_url(container)
+    try:
+        response = requests.get(url, timeout=TIMEOUT_CALL)
+        assert response.status_code == 200
+        assert "RStudio" in response.text
+    except ConnectionError:
+        assert False
 
 
 def test_shutdown():
+    """Test stopping the container."""
     container.stop()
     container.reload()
     assert container.status == "exited"
     container.remove()
+
+
+# Private methods
+
+
+def _get_url(container):
+    """Gets the URL of the container."""
+    ip = container.attrs["NetworkSettings"]["Networks"]["bridge"]["IPAddress"]
+    url = "http://{}:8787".format(ip)
+    return url
