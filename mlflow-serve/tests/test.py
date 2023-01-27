979e6fd9371a607cfc99aa0631acc61261186b98
sandstromviktor <v.sandstrom@icloud.com>
2023-01-27 17:50:15 +0100

    update

--- mlflow-serve/tests/test.py
+++ mlflow-serve/tests/test.py
@@ -1,18 +1,15 @@
+import os
 import mlflow
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 
-mlflow.start_run()
-X = np.array([-2, -1, 0, 1, 2, 1]).reshape(-1, 1)
-y = np.array([0, 0, 1, 1, 1, 0])
-lr = LogisticRegression()
-lr.fit(X, y)
+def create_test_model():
+    with mlflow.start_run():
+        X = np.array([-2, -1, 0, 1, 2, 1]).reshape(-1, 1)
+        y = np.array([0, 0, 1, 1, 1, 0])
+        lr = LogisticRegression()
+        lr.fit(X, y)
 
-model_info = mlflow.sklearn.save_model(lr, "test_model")
-mlflow.end_run()
+        mlflow.sklearn.save_model(lr, os.path.join('tests','test_model'))
 
-sklearn_pyfunc = mlflow.pyfunc.load_model(model_uri=model_info.model_uri)
 
-data = np.array([-4, 1, 0, 10, -2, 1]).reshape(-1, 1)
-
-predictions = sklearn_pyfunc.predict(data)
