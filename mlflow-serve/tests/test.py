3c15691a5c43f9f5a3e627db6322c63ac2725cc2
sandstromviktor <v.sandstrom@icloud.com>
2023-01-27 17:47:00 +0100

    added dummy model

--- mlflow-serve/tests/test.py
+++ mlflow-serve/tests/test.py
@@ -2,13 +2,14 @@ import mlflow
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 
-with mlflow.start_run():
-    X = np.array([-2, -1, 0, 1, 2, 1]).reshape(-1, 1)
-    y = np.array([0, 0, 1, 1, 1, 0])
-    lr = LogisticRegression()
-    lr.fit(X, y)
+mlflow.start_run()
+X = np.array([-2, -1, 0, 1, 2, 1]).reshape(-1, 1)
+y = np.array([0, 0, 1, 1, 1, 0])
+lr = LogisticRegression()
+lr.fit(X, y)
 
-    model_info = mlflow.sklearn.log_model(sk_model=lr, artifact_path="model")
+model_info = mlflow.sklearn.save_model(lr, "test_model")
+mlflow.end_run()
 
 sklearn_pyfunc = mlflow.pyfunc.load_model(model_uri=model_info.model_uri)
 
