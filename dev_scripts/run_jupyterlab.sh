f10e48b5940f106556d39f618934553984cadfb0
alfredeen <johan.alfredeen@gmail.com>
2023-02-07 09:14:19 +0100

    Fixed path to serve-jupyterlab.

--- dev_scripts/run_jupyterlab.sh
+++ dev_scripts/run_jupyterlab.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-docker build -t jupyterlab-dev-img -f ./jupyter-lab/Dockerfile.test ./jupyter-lab
+docker build -t jupyterlab-dev-img -f ./serve-jupyterlab/Dockerfile.test ./serve-jupyterlab
 python3 -m venv venv
 source ./venv/bin/activate
 python3 -m pip install --upgrade pip
-pip install -r ./jupyter-lab/tests/requirements.txt
+pip install -r ./serve-jupyterlab/tests/requirements.txt
 export IMAGE_NAME=jupyterlab-dev-img
-python3 -m pytest ./jupyter-lab/
+python3 -m pytest ./serve-jupyterlab/
