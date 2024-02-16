0ae87f6d866b98d9dee5ebe7f14d4caa8b2f48c5
sandstromviktor <v.sandstrom@icloud.com>
2024-02-16 13:41:17 +0100

    Added file manager image

---
+++ serve-filemanager/startup.sh
@@ -0,0 +1,6 @@
+#!/bin/ash
+
+/filebrowser config init
+/filebrowser users add $FB_USERNAME $FB_PASSWORD
+/filebrowser config set --branding.name "Serve File Manager" --branding.files "/home/serve/branding" --branding.disableExternal
+/filebrowser
