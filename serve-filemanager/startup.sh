4e65f9e0327f682f13680eb5ea28cefbfcabfc47
sandstromviktor <v.sandstrom@icloud.com>
2024-02-16 14:57:48 +0100

    Added no-auth

--- serve-filemanager/startup.sh
+++ serve-filemanager/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/ash
 
 /filebrowser config init
-/filebrowser users add test test
+/filebrowser users add $FB_USERNAME $FB_PASSWORD
 /filebrowser config set --branding.name "Serve File Manager" --branding.files "/home/serve/branding" --branding.disableExternal
 /filebrowser config set --auth.method=noauth
 /filebrowser
